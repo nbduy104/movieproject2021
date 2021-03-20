c809f61c09db3b1147ea2b15c0f453590a0b3211
Nin <nnninh.17ck1@gmail.com>
2021-03-20 10:44:37 +0700

    add: user

---
+++ src/layouts/dashboard-layout/ListUser.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ListUser = () => {
+    const listUser = [
+        {name:"Nguyen Nhat Ninh",birthday:"10/02/1999"},
+        {name:"Nguyen Nhat Ninh",birthday:"10/02/1999"},
+        {name:"Nguyen Nhat Ninh",birthday:"10/02/1999"},
+        {name:"Nguyen Nhat Ninh",birthday:"10/02/1999"},
+        {name:"Nguyen Nhat Ninh",birthday:"10/02/1999"},
+        {name:"Nguyen Nhat Ninh",birthday:"10/02/1999"},
+    ]
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ListUser;
--- src/layouts/dashboard-layout/Paperbase.js
+++ src/layouts/dashboard-layout/Paperbase.js
@@ -14,7 +14,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://movieproject-cv.netlify.app/">
-        Your Website
+        Link WepApp
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
